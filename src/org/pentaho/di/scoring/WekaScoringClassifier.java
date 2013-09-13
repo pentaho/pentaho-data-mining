1f9a7bd21262843a2712ab707e489684e7b5ae38
Pentaho Build Guy <buildgy@pentaho.com>
2013-09-13 14:47:30 +0000

    new branch after 5.0 release
    git-svn-id: svn://source.pentaho.org/svnkettleroot/plugins/WekaScoring/branches/5.0@19072 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/scoring/WekaScoringClassifier.java
+++ src/org/pentaho/di/scoring/WekaScoringClassifier.java
@@ -1,24 +1,26 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License, version 2 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+*
+* Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
+*/
 
 /*
- *    WekaScoringClassifier.java
- *    Copyright 2007 Pentaho Corporation.  All rights reserved. 
- *
- */
+*    WekaScoringClassifier.java
+*    Copyright 2007 Pentaho Corporation.  All rights reserved.
+*
+*/
 
 package org.pentaho.di.scoring;
 
