b54288a6c3f39cd07b2cff557387c8c5093fec39
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:12:05 -0500

    DEVO-11194: updated license and copyright headers

--- dm-commons/src/main/java/org/pentaho/dm/commons/LogAdapter.java
+++ dm-commons/src/main/java/org/pentaho/dm/commons/LogAdapter.java
@@ -1,15 +1,15 @@
-/*
- * Copyright (c) 2008 - 2017 Hitachi Vantara.  All rights reserved. 
- * This software was developed by Hitachi Vantara and is provided under the terms 
- * of the GNU Lesser General Public License, Version 2.1. You may not use 
- * this file except in compliance with the license. If you need a copy of the license, 
- * please go to http://www.gnu.org/licenses/lgpl-2.1.txt. The Original Code is Pentaho 
- * DM Commons.  The Initial Developer is Pentaho Corporation.
+/*! ******************************************************************************
  *
- * Software distributed under the GNU Lesser Public License is distributed on an "AS IS" 
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
- * the license for the specific language governing your rights and limitations.
- */
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
+
 
 /*
  *    LogAdapter.java
